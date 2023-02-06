8a36de3e1d26e9dd1f7e3131f987a0fccec2d486
James <jjack7543@gmail.com>
2023-02-05 21:14:23 -0500

    Fixed

--- Evil Sluts 4/Data/App_Data/Scripts/Game/game.js
+++ Evil Sluts 4/Data/App_Data/Scripts/Game/game.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f50542b7339477762a28fc848140d0fe6425780e1864d38a983f71d1d58ce2a
-size 7190
+oid sha256:0c2a93aa21741275e15a66101ed1ef4ee9ffcf4017c4f6c6bda7734abd3c2dab
+size 7191
