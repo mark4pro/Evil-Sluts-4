e8dc66a205399a03ce2fc6f46b3a8e53bec074a2
ghostyou246 <123477705+ghostyou246@users.noreply.github.com>
2023-02-11 22:23:56 -0500

    Update game.js

--- Evil Sluts 4/Data/App_Data/Scripts/Game/game.js
+++ Evil Sluts 4/Data/App_Data/Scripts/Game/game.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11eb5f2a9d734c3671d128438d8f3c8d28698f6fb92802f605bec81e470c69a0
-size 8553
+oid sha256:629ae024c07369b3422731505702ee3cc1d01e426f7abe0a09b44c904f5d94df
+size 8555
