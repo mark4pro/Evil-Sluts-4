b519dd40b36288a10f529dd83c5721391c2a3cee
James <jjack7543@gmail.com>
2023-10-22 02:22:42 -0400

    Update game.js

--- Evil Sluts 4/Data/App_Data/Scripts/Game/game.js
+++ Evil Sluts 4/Data/App_Data/Scripts/Game/game.js
@@ -90,8 +90,8 @@ function  dialogueUI(){
 	let option2Txt = null;
 	const update = () => {
 		if(getByNameTag(name("dialogueBox"), 1) == null && this.showing && valuate(gameState)){
-			this.dialogueBox = new Rectangle(8, new baseObject(true, new nameTag("dialogueBox", "UI"), Vec2(screen.resolution.x, 100), Vec2(screen.halfResolution.x, screen.resolution.y-50), new colorData("grey", 0.75)));
-			this.option1 = new Rectangle(8, new baseObject(true, new nameTag("option1", "UI"), Vec2(40, 30),this.dialogueBox.base.position.subV(Vec2(25, -50)), new colorData("darkgrey", 0.75), new shadow("black", FillVec2(5), 5)));
+			this.dialogueBox = new Rectangle(8, new baseObject(true, new nameTag("dialogueBox", "UI"), Vec2(screen.resolution.x, 100), Vec2(screen.halfResolution.x, screen.resolution.y-50), colorD("grey", 0.75)));
+			this.option1 = new Rectangle(8, new baseObject(true, new nameTag("option1", "UI"), Vec2(40, 30),this.dialogueBox.base.position.subV(Vec2(25, -50)), colorD("darkgrey", 0.75), shadow(FillVec2(5), "black", 5)));
 		}
 	}
 	addUpdate(update, "dialogueUI");
